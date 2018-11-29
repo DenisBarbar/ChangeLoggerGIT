cfa3cf7ec9fce77544167c0296cc0625a4407875
Unknown <barbar.denis@yandex.ru>
2018-11-29 10:11:30 +0300

    first item fixed, bad solution for adapter

--- ChangeLogger/app/src/main/java/com/fast_report/changelogger/ChangesFragment.java
+++ ChangeLogger/app/src/main/java/com/fast_report/changelogger/ChangesFragment.java
@@ -1,13 +1,17 @@
 package com.fast_report.changelogger;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -17,7 +21,14 @@ public class ChangesFragment extends Fragment {
     private ChangeAdapter mAdapter;
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("ChangesFragment", "onCreate");
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        Log.d("ChangesFragment", "onCreateView");
         ((MainActivity)getActivity()).setActionBarTitle("Changes");
         View view = inflater.inflate(R.layout.fragment_changes_list, container, false);
         mChangeRecyclerView = (RecyclerView) view.findViewById(R.id.changes_recycler_view);
@@ -27,15 +38,52 @@ public class ChangesFragment extends Fragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("ChangesFragment", "onStart");
+    }
+
+
+    @Override
     public void onResume(){
         super.onResume();
+        Log.d("ChangesFragment", "onResume");
         updateUI();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d("ChangesFragment", "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d("ChangesFragment", "onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d("ChangesFragment", "onDestroyView");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("ChangesFragment", "onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d("ChangesFragment", "onDetach");
     }
 
     private void updateUI(){
         ChangeLab changeLab = ChangeLab.get(getActivity());
         List<Change> crimes = changeLab.getChanges();
-        if(mAdapter == null){
+        if(mChangeRecyclerView.getAdapter() == null){
             mAdapter = new ChangeAdapter(crimes);
             mChangeRecyclerView.setAdapter(mAdapter);
         } else {
@@ -51,6 +99,7 @@ public class ChangesFragment extends Fragment {
         private TextView mGroupTextView;
         private TextView mAuthorTextView;
         private TextView mChangedTextView;
+        //private Button mDeleteButton;
 
         public void bindChange(Change change) {
             mChange = change;
@@ -68,6 +117,16 @@ public class ChangesFragment extends Fragment {
             mGroupTextView = (TextView) itemView.findViewById(R.id.list_item_change_group);
             mAuthorTextView = (TextView) itemView.findViewById(R.id.list_item_change_author);
             mChangedTextView = (TextView) itemView.findViewById(R.id.list_item_changed_text);
+            /*
+            mDeleteButton = (Button) itemView.findViewById(R.id.delete_button);
+            mDeleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mAdapter.mChanges.remove(mChange.getId());
+                    updateUI();
+                }
+            });
+            */
         }
 
     }
